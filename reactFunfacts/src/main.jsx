92eeb91549fff1cdee86f86639414720e7fceb40
SADU VENKATESH <saduvenkatesh141205@gmail.com>
2024-12-19 20:53:46 +0530

    static page of react-fun-facts

--- reactFunfacts/src/main.jsx
+++ reactFunfacts/src/main.jsx
@@ -1,7 +1,18 @@
 import { createRoot } from "react-dom/client"
-
+import "/src/index.css"
 const root = createRoot(document.getElementById("root"))
 
 root.render(
-  <h1>hello react !</h1>
+  <div>
+    <img src="/src/assets/react.svg" alt="react-logo" width="40px" />
+    <h1>Fun facts about React!</h1>
+
+    <ul>
+      <li>Was first release in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 200k stars on GitHUb</li>
+      <li>Is maintained by Meta</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
 )
