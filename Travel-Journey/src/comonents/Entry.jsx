bc840d5a108ae2fdccc0b89e8118b0b50bb97bf4
SADU VENKATESH <saduvenkatesh141205@gmail.com>
2024-12-23 18:50:48 +0530

    passing data through props

--- Travel-Journey/src/comonents/Entry.jsx
+++ Travel-Journey/src/comonents/Entry.jsx
@@ -1,21 +1,21 @@
 import "./Entry.css"
-export default function Entry(){
-    
-    
+export default function Entry(props){
 
+    
     return (
         
             <>
                 <article className="card-container">
                     <div className="main-img-container">
-                    <img src="https://tse3.mm.bing.net/th?id=OIP.fgk3tfnDt4aP3WN3gcfREAHaFj&pid=Api&P=0&h=220" alt="main-img" className="main-img" />
+                    <img src={props.img} alt="main-img" className="main-img" />
                     </div>
-                    <div>
+                    <div className="card-description">
                         <img src="https://tse4.mm.bing.net/th?id=OIP.jsBQL04RxeNPuuCRzQlpkAHaHa&pid=Api&P=0&h=220" alt="map-icon" className="map-icon" />
-                        <span><a href="https://maps.app.goo.gl/HBiZRC3Bgm5S2G1X7">View on Google Maps</a></span>
-                        <h1>Golconda Fort</h1>
+                        <span><a href={props.address}>View on Google Maps</a></span>
+                        
+                        <h1>{props.title}</h1>
                         <p><strong>12 Jan 2024-24 to Jan 2024</strong></p>
-                        <p>Golconda is a fortified citadel and ruined city located on the western outskirts of Hyderabad, Telangana, India. The fort was originally built by Kakatiya ruler Pratāparudra in the 11th century out of mud walls.</p>
+                        <p>{props.details}</p>
                     </div>
                 </article>
             </>
