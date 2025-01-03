d085103b970488202aaa28b05f81ca56d788be31
SADU VENKATESH <saduvenkatesh141205@gmail.com>
2025-01-03 11:04:50 +0530

    passing props as a object

--- Travel-Journey/src/App.jsx
+++ Travel-Journey/src/App.jsx
@@ -4,17 +4,15 @@ import data from "./data"
 
 export default function App(){
 
-  const entryElements = (data).map((entry)=>{
+  const entryElements = data.map((entry)=>{
       
-      <Entry
-               img={entry.img.src}
-               alt={entry.img.alt}
-               title={entry.title}
-               country={entry.title}
-               googleMapsLink={entry.googleMapsLink}
-               dates={entry.dates}
-               text={entry.text}
+      return (
+        
+        <Entry
+              key={entry.id}
+               entry={entry}
       />
+      )
   })
 
   return (
--- Travel-Journey/src/comonents/Entry.jsx
+++ Travel-Journey/src/comonents/Entry.jsx
@@ -1,21 +1,21 @@
 import "./Entry.css"
 export default function Entry(props){
 
-    
+    console.log(props)
     return (
         
             <>
                 <article className="card-container">
                     <div className="main-img-container">
-                    <img src={props.img} alt={props.alt} className="main-img" />
+                    <img src={props.entry.img.src} alt={props.entry.img.alt} className="main-img" />
                     </div>
                     <div className="card-description">
                         <img src="https://tse4.mm.bing.net/th?id=OIP.jsBQL04RxeNPuuCRzQlpkAHaHa&pid=Api&P=0&h=220" alt="map-icon" className="map-icon" />
-                        <span><a href={props.googleMapsLink}>View on Google Maps</a></span>
+                        <span><a href={props.entry.googleMapsLink}>View on Google Maps</a></span>
                         
                         <h1>{props.entry.title}</h1>
-                        <p><strong>{props.dates}</strong></p>
-                        <p>{props.text}</p>
+                        <p><strong>{props.entry.dates}</strong></p>
+                        <p>{props.entry.text}</p>
                     </div>
                 </article>
             </>
