c34a6dcf682987c8b8a2d396f65902c943b01239
SADU VENKATESH <saduvenkatesh141205@gmail.com>
2025-01-03 12:12:02 +0530

    delete some unwanted files

---
+++ chef-claude/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css"
+export default function App(){
+    return(
+        <>
+            <h1>this is from app.jsx</h1>
+        </>
+    )
+}
---
+++ chef-claude/src/main.jsx
@@ -0,0 +1,9 @@
+import App from "./App"
+
+import { createRoot } from "react-dom/client"
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+    <App/>
+)
