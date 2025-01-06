b61a336c338f5f9b6118c1f4d4d1c4a3cce459c2
SADU VENKATESH <saduvenkatesh141205@gmail.com>
2025-01-06 11:02:26 +0530

    toggling state

--- yesOrNo/src/App.jsx
+++ yesOrNo/src/App.jsx
@@ -4,22 +4,18 @@ export default function App () {
    
     const [isGoingOut, setIsGoingOut] = useState(true)
     
-    function handleClick(){
-        
-        if (isGoingOut) 
-         setIsGoingOut(false)
-        else
-        setIsGoingOut(true)
-
-
+    function changeMind(){
+        setIsGoingOut(prev => !prev)
     }
-
-
-
     return(
         <>
                <h1>Do you feel like going outside today ? </h1>
-               <button onClick={handleClick} >{isGoingOut ? "Yes" : "No"}</button>
+               {/* <button onClick={()=>{setIsGoingOut(!isGoingOut)}} >{isGoingOut ? "Yes" : "No"}</button> */}
+               <button onClick={changeMind}
+                       className="value"
+                       aria-label={`current answer is ${isGoingOut ? "Yes" : "NO"}. Click to change it`}
+               > {isGoingOut ? "Yes" : "No"}
+               </button>
         </>
     )
 }
